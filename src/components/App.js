0fa2018dbe922589c669f481119e8a02c3f86899
beafig <beafigueroa89@gmail.com>
2023-03-21 14:57:45 +0100

    Add react starter kit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+/* SECCIÓN DE IMPORT: de React, componentes, SASS, imágenes*/
+
+import '../styles/App.scss';
+
+/* SECCIÓN DEL COMPONENTE: variables de estado, efectos, funciones handler, funciones y variables para pintar el HTML y el HTML*/
+function App() {
+
+  return <div className="App">{/* Aquí va el HTML */}</div>;
+}
+
+export default App;
