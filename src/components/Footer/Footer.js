5e43a55b1964ea49a0e2da76fafbcd144604828a
beafig <beafigueroa89@gmail.com>
2023-03-22 01:13:50 +0100

    Add styles in langing and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,11 @@
+import '../../styles/layout/Footer.scss'
+import logo from '../../images/harryPotter.png'
+
 function Footer() {
   return (
-    <footer>
-      <p>Mi página</p>
+    <footer className='footer'>
+      <p className='footer__text'>Mi pagina</p>
+      <img src={logo} alt="logo Harry Potter" title='logo Harry Potter' className='footer__logo' />
     </footer>
   )
 }
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 
+import introMusic from '../../audio/intro.mp3'
+import '../../styles/layout/Landing.scss'
+
 function Landing() {
   return (
-    <>
-      <h2>Bienvenides</h2>
-      <Link to="/characters/">
-        Ir a personajes
+    <section className='landing'>
+      <Link to="/characters/" >
+        <input type="button" value="Ver personajes" className='landing__btn' />
       </Link>
-    </>
+      <audio autoPlay loop>
+        <source src={introMusic} type="audio/mp3" />
+      </audio>
+    </section>
   )
 }
 export default Landing;
