9af9eb13ef2816cc77a4a95c29610ae390b53976
beafig <beafigueroa89@gmail.com>
2023-03-22 15:02:32 +0100

    Add not found route and styles

--- src/components/List/NotFound.js
+++ src/components/List/NotFound.js
@@ -1,6 +1,19 @@
+import { Link } from 'react-router-dom';
+import Header from '../Header/Header'
+import '../../styles/layout/NotFound.scss'
+
 function NotFound() {
   return (
-    <h2>No existe</h2>
+    <>
+      <Header />
+      <section className="notFound">
+        <Link to="/characters/">
+          <input type="button" value="Go to characters" className='notFound__btn' />
+        </Link>
+        <h2 className="notFound__text">The page you are trying to visit doesn't exist yet.</h2>
+        <img src="http://img.qdaily.com/uploads/20180628182235eiRXashVlcWnz1q3.gif-WebpWebW640" alt="gif Dumbledore" title="gif Dumbledore" className="notFound__img" />
+      </section>
+    </>
   )
 }
 export default NotFound;
